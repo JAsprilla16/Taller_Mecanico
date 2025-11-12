ce6325394d845ff6cca1ec688fe35d9ea9a9e7c0
Juan Arcos <arcosasprillajose@gmail.com>
2025-11-11 19:52:20 -0500

    Actualizo el main.ts

---
+++ src/environments/firebase.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  apiKey: "AIzaSyA-3iuj21Nbi_TiN0wPxqBVfQrgMJLbh3M",
+  authDomain: "taller-mecanico-cf9dc.firebaseapp.com",
+  projectId: "taller-mecanico-cf9dc",
+  storageBucket: "taller-mecanico-cf9dc.firebasestorage.app",
+  messagingSenderId: "270005668687",
+  appId: "1:270005668687:web:efa5e7cff79c1b1b63933a"
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { importProvidersFrom } from '@angular/core';
+import { environment } from './environments/firebase';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment)),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth())
+    )
+  ]
+}
+    )
   .catch((err) => console.error(err));
